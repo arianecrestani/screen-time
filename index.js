a631cbdd1de0ab715dcafc91f6b5a518b20930dd
Ariane Crestani <ariane.mrv@gmail.com>
2021-10-17 20:36:58 +0200

    some changes

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const loading = () => {
     const msg = document.getElementById('msg');
     const img = document.getElementById('image');
     const data = new Date()
-    // const hour = data.getHours()
-    const hour = 20
+    const hour = data.getHours()
+    // const hour = 8
+   
 
     msg.innerHTML = `now is ${ hour } hour`
+
     
     if(hour >= 0 && hour < 12) {
         img.src ='../screen-time/images/mornig.png'
+        document.body.style.background = '#e2cd9f'
     }else if (hour > 13 && hour <= 18){
         img.src ='./images/afternoon.png'
+        document.body.style.background = '#b9846f'
     }else {
         img.src ='./images/night.png'
+        document.body.style.background = '#515154'
     }
 }
