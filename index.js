e0a3542b437e9bc12210bde4d3ecf0ef0c28682b
Ariane Crestani <ariane.mrv@gmail.com>
2021-10-17 20:13:39 +0200

    set hours for the imagens

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const msg = document.getElementById('msg');
-const img = document.getElementById('img');
+
+
+const loading = () => {
+    const msg = document.getElementById('msg');
+    const img = document.getElementById('image');
+    const data = new Date()
+    const hour = data.getHours()
+    msg.innerHTML = `now is ${ hour } hour`
+    
+    if(hour >= 0 && hour < 12) {
+        img.src ='./images/mornig.png'
+    }else if (hour > 12 && hour <= 18){
+        img.src ='./images/afternoon.png'
+    }else {
+        img.src ='./images/night.png'
+    }
+}
