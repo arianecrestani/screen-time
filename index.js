9649c42ce7c580356164b26157c3d2b2c0122ab1
Ariane Crestani <ariane.mrv@gmail.com>
2021-10-17 20:23:26 +0200

    add changes

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const loading = () => {
     const msg = document.getElementById('msg');
     const img = document.getElementById('image');
     const data = new Date()
-    const hour = data.getHours()
+    // const hour = data.getHours()
+    const hour = 20
+
     msg.innerHTML = `now is ${ hour } hour`
     
     if(hour >= 0 && hour < 12) {
-        img.src ='./images/mornig.png'
-    }else if (hour > 12 && hour <= 18){
+        img.src ='../screen-time/images/mornig.png'
+    }else if (hour > 13 && hour <= 18){
         img.src ='./images/afternoon.png'
     }else {
         img.src ='./images/night.png'
