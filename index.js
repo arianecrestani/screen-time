d8c7df7ddbe4273bd0a2e7eeaecdc86dcbb8f31c
Ariane Crestani <ariane.mrv@gmail.com>
2021-10-18 10:56:11 +0200

    add some changes

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const loading = () => {
   const msg = document.getElementById("msg");
   const dayOfWeek = document.getElementById("dayOfWeek");
-  const img = document.getElementById("image");
+  const img = document.getElementById("picture");
   const dt = new Date();
   const time = dt.getHours() + ":" + dt.getMinutes();
   const dayWeek = dt.getDay();
+
+  msg.innerHTML = `now is ${time} hour`;
+
+  if (time >= 0 && time < 12) {
+    img.src = "./images/mornig.png";
+    document.body.style.background = "#e2cd9f";
+  } else if (time > 13 && time <= 18) {
+    img.src = "./images/afternoon.png";
+    document.body.style.background = "#b9846f";
+  } else {
+    img.src = "./images/night.png";
+    document.body.style.background = "#515154";
+  }
 
   switch (dayWeek) {
     case 0:
@@ -30,16 +43,5 @@ const loading = () => {
       break;
   }
 
-  msg.innerHTML = `now is ${time} hour`;
-
-  if (time >= 0 && time < 12) {
-    img.src = "../screen-time/images/mornig.png";
-    document.body.style.background = "#e2cd9f";
-  } else if (time > 13 && time <= 18) {
-    img.src = "./images/afternoon.png";
-    document.body.style.background = "#b9846f";
-  } else {
-    img.src = "./images/night.png";
-    document.body.style.background = "#515154";
-  }
+  
 };
